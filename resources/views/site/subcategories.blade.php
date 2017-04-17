ad0f010134a35403116d480fb120151d55fe5de1
AnastasiiaPetko <nasskapetko@gmail.com>
2017-04-17 17:53:19 +0300

    страница подкатегорий в моб версии

--- resources/views/site/subcategories.blade.php
+++ resources/views/site/subcategories.blade.php
@@ -18,3 +18,19 @@
             @endforeach
         </ul>
 @stop
+@section('mobile')
+ <div class="category-title mt10">
+            {{$category->name}}
+        </div>
+        <div class="category-title-line">
+        </div>
+
+        <div class="category-description">
+            {!! $category->description !!}
+        </div>
+        <ul class="subcat-total-list">
+            @foreach($subcategories as $subcategory)
+                <li><a href="/subcategory/{{$subcategory->url}}">{{ $subcategory->name }}</a></li>
+            @endforeach
+        </ul>
+@stop
