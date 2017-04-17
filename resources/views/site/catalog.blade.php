644903734bb29edc0801d56f5f8a26cbeec75f39
AnastasiiaPetko <nasskapetko@gmail.com>
2017-04-17 16:30:59 +0300

    Revert "шото удалила"
    This reverts commit 0ead9f890646f910a850a2e172713cb3e8f478c8.
    
    #

--- resources/views/site/catalog.blade.php
+++ resources/views/site/catalog.blade.php
@@ -2,7 +2,147 @@
 
 @section('header')
 
+<script>
+//     var katalogButtonOpen = document.getElementsByClassName('category-button');
+//     var katalogButtonOpenMobile = document.getElementsByClassName('category-button-mobile');
+//     var subCategory = document.createElement('div');
+//     subCategory.setAttribute('id', 'subCategory');
+//     function buttonClear(){
+//         for(i =0; i< katalogButtonOpen.length; i++){
+//             katalogButtonOpen[i].classList.remove("to-katalog-active");
+//         }
+//     };
+//     function hide(item) {
+//      document.getElementById('subCategory').parentNode.removeChild(document.getElementById('subCategory'));
+//      item.style.display = 'none';
+//      buttonClear();
+//  }
+//  window.addEventListener("load", function(){
 
+//     function insertAfter( node, referenceNode ) {
+//         if ( !node || !referenceNode ) return;
+//         var parent = referenceNode.parentNode, nextSibling = referenceNode.nextSibling;
+//         if ( nextSibling && parent ) {
+//             parent.insertBefore(node, referenceNode.nextSibling);
+//         } else if ( parent ) {
+//             parent.appendChild( node );
+//         }
+//     };
+
+//     for(i=0; i < katalogButtonOpen.length; i++){
+//         (function(index) {
+
+//             katalogButtonOpen[index].addEventListener("click", function() {
+//              buttonClear();
+//              this.className += " to-katalog-active";
+
+//              categoryid = this.dataset.categoryid;
+//              categoryUrl = document.getElementById('category-' + categoryid).value;
+//              ajax('/ajax/subcategories-by-category-id?id=' + categoryid, function(data) {
+//                 data = JSON.parse(data);
+//                 document.getElementsByClassName('subcategories-list')[0].innerHTML = '';
+//                 if(!data.length) {
+//                     document.getElementsByClassName('subcategories-list')[0].innerHTML = 'У этой категории нет подкатегорий';
+//                 } else {
+//                     for (var i = 0; i < data.length; i++) {
+//                         document.getElementsByClassName('subcategories-list')[0].innerHTML += '<a href="/category/' + categoryUrl + '?subcategory=' + data[i].id + '"><div class="subcat-item subcat-hover fl">' +
+//                         '<div class="subcat-image fl">' +
+//                         '<img src="site/images/icon-catecory-list.png" alt="">' +
+//                         '</div>' +
+//                         '<div class="subcat-text  fl">' +
+//                         data[i].name +
+//                         '</div>' +
+//                         '</div></a>';
+//                     };
+//                 }
+
+
+
+//                 curent = Math.ceil((index + 1) / 4);   
+//                 var subCategoryBlock = document.getElementById('subcategory-katalog');
+//                 subCategory.innerHTML = subCategoryBlock.innerHTML;
+//                 curIndex = curent*4 - 1;
+//                 if(!document.getElementsByClassName('category-button')[curIndex]) {
+//                     list = document.getElementsByClassName('category-button');
+//                     curIndex = list.length - 1;
+//                 }
+//                 curItem = document.getElementsByClassName('category-button')[curIndex].parentNode.parentNode;
+
+//                 insertAfter(subCategory, curItem);
+//                     // subCategoryBlock.style.display="block";
+//                     subCategory.style.display="block";
+//                     var elemImage = document.getElementsByClassName("subcat-hover");
+//                     for (i=0; i < elemImage.length; i++){
+//                         elemImage[i].addEventListener("mouseover", function(){
+//                             this.getElementsByTagName('img')[0].setAttribute("src", "{{ url('site/images/icon-catecory-list-active.png') }}");
+//                         });
+//                         elemImage[i].addEventListener("mouseout", function(){
+//                             this.getElementsByTagName('img')[0].setAttribute("src", "{{ url('site/images/icon-catecory-list.png') }}");
+//                         });
+//                     }
+
+
+//                 });
+
+//          });
+
+
+//         })(i);
+//     };
+
+
+//     for(i=0; i < katalogButtonOpenMobile.length; i++){
+//         (function(index) {
+
+//             katalogButtonOpenMobile[i].addEventListener("click", function() {
+//              buttonClear();
+//                     // this.className += " to-katalog-active";
+
+//                     categoryid = this.dataset.categoryid;
+//                     categoryUrl = document.getElementById('category-' + categoryid).value;
+//                     ajax('/ajax/subcategories-by-category-id?id=' + categoryid, function(data) {
+//                         data = JSON.parse(data);
+//                         document.getElementsByClassName('subcategories-list')[0].innerHTML = '';
+//                         if(!data.length) {
+//                             document.getElementsByClassName('subcategories-list')[0].innerHTML = 'У этой категории нет подкатегорий';
+//                         } else {
+//                             for (var i = 0; i < data.length; i++) {
+//                                 document.getElementsByClassName('subcategories-list')[0].innerHTML += '<a href="/category/' + categoryUrl + '?subcategory=' + data[i].id + '"><div class="subcat-item subcat-hover fl">' +
+//                                 '<div class="subcat-image fl">' +
+//                                 '<img src="site/images/icon-catecory-list.png" alt="">' +
+//                                 '</div>' +
+//                                 '<div class="subcat-text  fl">' +
+//                                 data[i].name +
+//                                 '</div>' +
+//                                 '</div></a>';
+//                             };
+//                         }
+
+
+//                         var curIndex = Math.ceil(index); 
+//                         var subCategoryBlock = document.getElementById('subcategory-katalog');
+//                         subCategory.innerHTML = subCategoryBlock.innerHTML;
+//                         curItem = document.getElementsByClassName('category-button-mobile')[curIndex].parentNode.parentNode;
+//                         insertAfter(subCategory, curItem);
+//                     // subCategoryBlock.style.display ="block";
+//                     subCategory.style.display="block";
+
+//                 });
+
+
+
+//                 });
+
+
+//         })(i);
+//     };
+
+// });
+
+
+// });
+
+</script>
 
 @stop
 
